011b6cc1fc743e86369dcdf0660bd6ddc183668c
Karim R. Habboub <karim.2360967@gmail.com>
2023-07-31 17:58:52 +0300

    create custom dialog

--- lib/shared/style/colors.dart
+++ lib/shared/style/colors.dart
@@ -1,21 +1,39 @@
-import 'package:hexcolor/hexcolor.dart';
+import 'package:flutter/material.dart';
 
-   HexColor myBackgroundColor = HexColor("#F6F6F6");
-   HexColor myFCFCFCColor = HexColor("#FCFCFC");
-   HexColor myEEEEEEColor = HexColor("#EEEEEE");
-   HexColor myColor = HexColor("#F58632");
-   HexColor myBlackColor = HexColor("#394144");
-   HexColor myBlackBorderColor = HexColor("#66696A");
-   HexColor my828282Color = HexColor("#828282");
-   HexColor myCFCFCFColor = HexColor("#CFCFCF");
-   HexColor my929292Color = HexColor("#929292");
-   HexColor myFF5B50Color = HexColor("#FF5B50");
-   HexColor my66696AColor = HexColor("#66696A");
-   HexColor myDEDEDEColor = HexColor("#DEDEDE");
-   HexColor myFFFFFFColor = HexColor("#FFFFFF");
+   // HexColor myBackgroundColor = HexColor("#F6F6F6");
+   // HexColor myFCFCFCColor = HexColor("#FCFCFC");
+   // HexColor myEEEEEEColor = HexColor("#EEEEEE");
+   // HexColor myColor = HexColor("#F58632");
+   // HexColor myBlackColor = HexColor("#394144");
+   // HexColor myBlackBorderColor = HexColor("#66696A");
+   // HexColor my828282Color = HexColor("#828282");
+   // HexColor myCFCFCFColor = HexColor("#CFCFCF");
+   // HexColor my929292Color = HexColor("#929292");
+   // HexColor myFF5B50Color = HexColor("#FF5B50");
+   // HexColor my66696AColor = HexColor("#66696A");
+   // HexColor myDEDEDEColor = HexColor("#DEDEDE");
+   // HexColor myFFFFFFColor = HexColor("#FFFFFF");
+   // //Error item
+   // HexColor myC3C5CEErrorColor = HexColor("#C3C5CE");
+   // HexColor myE7E7E7ErrorColor = HexColor("#E7E7E7");
+
+
+   Color myBackgroundColor = const Color(0xFFF6F6F6);
+   Color myFCFCFCColor = const Color(0xFFFCFCFC);
+   Color myEEEEEEColor = const Color(0xFFEEEEEE);
+   Color myColor = const Color(0xFFF58632);
+   Color myBlackColor = const Color(0xFF394144);
+   Color myBlackBorderColor = const Color(0xFF66696A);
+   Color my828282Color = const Color(0xFF828282);
+   Color myCFCFCFColor = const Color(0xFFCFCFCF);
+   Color my929292Color = const Color(0xFF929292);
+   Color myFF5B50Color = const Color(0xFFFF5B50);
+   Color my66696AColor = const Color(0xFF66696A);
+   Color myDEDEDEColor = const Color(0xFFDEDEDE);
+   Color myFFFFFFColor = const Color(0xFFFFFFFF);
    //Error item
-   HexColor myC3C5CEErrorColor = HexColor("#C3C5CE");
-   HexColor myE7E7E7ErrorColor = HexColor("#E7E7E7");
+   Color myC3C5CEErrorColor = const Color(0xFFC3C5CE);
+   Color myE7E7E7ErrorColor = const Color(0xFFE7E7E7);
 
 
 
